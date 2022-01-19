e0be398baa2d21f527ee67670ed470e77107eeda
Michal Lukaszek <mlukaszek@gmail.com>
2022-01-19 20:41:14 +0100

    Homebrew lives in different directories on Intel than on M1

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,14 @@
+BREW="$(brew --prefix)"
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+if [[ ! "$PATH" == *$BREW/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$BREW/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$BREW/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+source "$BREW/opt/fzf/shell/key-bindings.zsh"
